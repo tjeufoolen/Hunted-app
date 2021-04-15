348c46146a93750d251dabcec129f41d51324cf4
killermi200 <killermi200@gmail.com>
2021-04-15 19:44:53 +0200

    Nothing happend here either 🤭

--- lib/models/Game.dart
+++ lib/models/Game.dart
@@ -21,22 +21,3 @@ class Game {
         'minutes': minutes,
       };
 }
-
-// {
-//   id: 1,
-//   gameId: 3,
-//   code: $2a$1-0$OPj-1ZwWu-3JYYE-nhHZZ-y02uv-vNET0-mrqw7-0u.O1-YdrFU-.v6fF-9VkxW,
-//   playerRole: 0,
-//   outOfTheGame: false,
-//   locationId: null,
-//   createdAt: 2021-04-15T10:44:04.000Z,
-//   GameId: 3,
-//   Game: {
-//     id: 3,
-//     userId: 1,
-//     startAt: 2021-04-15T18:00:00.000Z,
-//     minutes: 180,
-//     layoutTemplateId: 0,
-//     createdAt: 2021-04-15T10:44:04.000Z
-//     }
-// }
