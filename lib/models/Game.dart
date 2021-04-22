49a97c56c72b4d9e03b61433a78f372d8c127c08
killermi200 <killermi200@gmail.com>
2021-04-22 12:00:02 +0200

    inbetween

--- lib/models/Game.dart
+++ lib/models/Game.dart
@@ -1,23 +1,29 @@
+import 'package:hunted_app/models/GameLocation.dart';
+import 'dart:convert' as convert;
+
 class Game {
   final int id;
-  final int userId;
   final DateTime startAt;
   final int minutes;
+  final List<GameLocation> gameLocations;
 
-  Game({this.id, this.userId, this.startAt, this.minutes});
+  Game({this.id, this.startAt, this.minutes, this.gameLocations});
 
   factory Game.fromJson(Map<String, dynamic> json) {
     return Game(
         id: json['id'],
-        userId: json['userId'],
         startAt: DateTime.parse(json['startAt']),
-        minutes: json['minutes']);
+        minutes: json['minutes'],
+        gameLocations: (convert.json.decode(json['gameLocations']))
+            .map((i) => GameLocation.fromJson(i))
+            .toList());
   }
 
   Map<String, dynamic> toJson() => {
         'id': id,
-        'userId': userId,
         'startAt': startAt.toIso8601String(),
         'minutes': minutes,
+        'gameLocations': convert.json
+            .encode(List<dynamic>.from(gameLocations.map((i) => i.toJson())))
       };
 }
