129826389d079b9b5f95bac55ffcdc6867d9c3bd
killermi200 <killermi200@gmail.com>
2021-05-13 12:08:45 +0200

    Map of game area is now drawn

--- lib/widgets/MapWidgets/GameMap.dart
+++ lib/widgets/MapWidgets/GameMap.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hunted_app/models/Game.dart';
 import 'package:hunted_app/models/Player.dart';
-import 'package:hunted_app/util/ImageHelper.dart';
 import 'package:hunted_app/util/MarkerFactory.dart';
 import 'package:location/location.dart';
 
@@ -20,14 +20,11 @@ class GameMap extends StatefulWidget {
 class _GameMapController extends State<GameMap> {
   @override
   Widget build(BuildContext context) {
-    if (widget?.loggedInPlayer?.game?.gameLocations != null) {
-      MarkerFactory()
-          .createAll(widget.loggedInPlayer.game.gameLocations)
-          .then((value) {
-        setState(() {
-          _markers = value.toSet();
-        });
-      });
+    final Game currentGame = widget?.loggedInPlayer?.game;
+
+    if (currentGame != null) {
+      _setGameArea(currentGame);
+      _setGameLocations(currentGame);
     }
 
     return _GameMapView(this);
@@ -38,6 +35,7 @@ class _GameMapController extends State<GameMap> {
   Location _location = Location();
 
   Set<Marker> _markers = {};
+  Set<Circle> _gameArea = {};
   // Only used for initial loading, will be replaced as soon as user location is fetched.
   LatLng _playerPosition = LatLng(51.6978162, 5.3036748);
 
@@ -63,6 +61,39 @@ class _GameMapController extends State<GameMap> {
     _location.onLocationChanged().listen((LocationData newLocation) {
       _playerPosition = LatLng(newLocation.latitude, newLocation.longitude);
     });
+  }
+
+  void _setGameLocations(Game currentGame) {
+    if (currentGame?.gameLocations != null) {
+      MarkerFactory().createAll(currentGame.gameLocations).then((value) {
+        setState(() {
+          _markers = value.toSet();
+        });
+      });
+    }
+  }
+
+  void _setGameArea(Game currentGame) {
+    bool gameHasLatitude = currentGame.gameAreaLatitude != null;
+    bool gameHasLongitude = currentGame.gameAreaLongitude != null;
+    bool gameHasAreaRadius = currentGame.gameAreaRadius != null;
+
+    if (gameHasLatitude && gameHasLongitude && gameHasAreaRadius) {
+      Set<Circle> newGameArea = {
+        Circle(
+          circleId: CircleId('gameArea'),
+          center: LatLng(
+              currentGame.gameAreaLatitude, currentGame.gameAreaLongitude),
+          radius: currentGame.gameAreaRadius.toDouble(),
+          strokeWidth: 5,
+          strokeColor: Color.fromARGB(100, 86, 247, 64),
+          fillColor: Color.fromARGB(14, 86, 247, 64),
+        )
+      };
+      setState(() {
+        _gameArea = newGameArea;
+      });
+    }
   }
 
   // Moves the camera to the current location of the player
@@ -93,6 +124,7 @@ class _GameMapView extends WidgetView<GameMap, _GameMapController> {
         zoomControlsEnabled: false,
         buildingsEnabled: false,
         markers: state._markers,
+        circles: state._gameArea,
       ),
     );
   }
