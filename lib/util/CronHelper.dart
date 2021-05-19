48dd336932f0082b98b44089d7321e97d6ed6a3a
killermi200 <killermi200@gmail.com>
2021-05-19 15:40:19 +0200

    Fixed player locations not being shown

--- lib/util/CronHelper.dart
+++ lib/util/CronHelper.dart
@@ -3,24 +3,22 @@ import 'package:hunted_app/models/Player.dart';
 import 'package:hunted_app/services/SocketService.dart';
 import 'package:location/location.dart';
 
-
 class CronHelper {
-
   Location _location = new Location();
   SocketService _socketService = new SocketService();
 
-   Cron initializeCron(Player loggedInPlayer){
-     Cron cron = new Cron();
-     cron.schedule(new Schedule.parse('*/1 * * * *'), () async {
-       _location.getLocation().then((newLocation) {
-         var message = {
-           "id": loggedInPlayer.id,
-           "latitude": newLocation.latitude,
-           "longitude": newLocation.longitude
-         };
-         _socketService.emitData('send_location', message);
-       });
-     });
-     return cron;
-   }
+  Cron initializeCron(Player loggedInPlayer) {
+    Cron cron = new Cron();
+    cron.schedule(new Schedule.parse('*/30 * * * * *'), () async {
+      _location.getLocation().then((newLocation) {
+        var message = {
+          "id": loggedInPlayer.id,
+          "latitude": newLocation.latitude,
+          "longitude": newLocation.longitude
+        };
+        _socketService.emitData('send_location', message);
+      });
+    });
+    return cron;
+  }
 }
