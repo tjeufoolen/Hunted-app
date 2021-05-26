9ead1430fe2bac2494d8652dc9bb75156836c41e
ThatOneGuyRick <rickberg98@live.nl>
2021-05-26 15:51:22 +0200

    basic on click

--- lib/models/Markers/TreasureMarker.dart
+++ lib/models/Markers/TreasureMarker.dart
@@ -1,12 +1,22 @@
+import 'package:flutter_session/flutter_session.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hunted_app/models/Markers/CustomMarker.dart';
+import 'package:hunted_app/models/Player.dart';
 
 class TreasureMarker extends CustomMarker {
-  TreasureMarker(MarkerId markerId, LatLng location)
-      : super(markerId, location, 'map-markers/treasure_marker.png', 75);
+  int _id;
+
+  TreasureMarker(MarkerId markerId, LatLng location, int id)
+      : super(markerId, location, 'map-markers/treasure_marker.png', 75){
+        _id = id;
+      }
 
   @override
-  void onClick() {
+  Future<void> onClick() async {
     // TODO: implement onClick
+    Player player = Player.fromJson(await FlutterSession().get("LoggedInPlayer"));
+
+    print("id:");
+    print(_id);
   }
 }
