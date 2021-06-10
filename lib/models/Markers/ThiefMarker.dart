483fb9ac1a5df7cc9f039e177ea2d195d669f33b
ThatOneGuyRick <rickberg98@live.nl>
2021-06-10 11:10:23 +0200

    made thief marker clickable

--- lib/models/Markers/ThiefMarker.dart
+++ lib/models/Markers/ThiefMarker.dart
@@ -1,12 +1,23 @@
+import 'package:flutter_session/flutter_session.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hunted_app/models/Markers/CustomMarker.dart';
+import 'package:hunted_app/models/Player.dart';
+import 'package:hunted_app/services/SocketService.dart';
 
 class ThiefMarker extends CustomMarker {
-  ThiefMarker(MarkerId markerId, LatLng location)
-      : super(markerId, location, 'map-markers/thief_marker.png', 75);
+  int _id;
+
+  ThiefMarker(MarkerId markerId, LatLng location, int id)
+      : super(markerId, location, 'map-markers/thief_marker.png', 75){
+        _id = id;
+      }
 
   @override
-  void onClick() {
-    // TODO: implement onClick
+  Future<void> onClick() async {
+    Player player = Player.fromJson(await FlutterSession().get("LoggedInPlayer"));
+    SocketService socket = SocketService();
+    if(player.playerRole == PlayerRolesEnum.POLICE){
+      socket.emitData('arrest_thief', {"playerId": player.id, "thiefId": _id});
+    }
   }
 }
